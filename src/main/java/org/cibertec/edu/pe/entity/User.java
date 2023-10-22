0e11a0030048d55b2ef5406385ed548fb8ebf174
rasec1106 <elch.herrera@gmail.com>
2023-10-22 01:45:09 -0500

    SCRUM-25 Add user class

---
+++ src/main/java/org/cibertec/edu/pe/entity/User.java
@@ -0,0 +1,24 @@
+package org.cibertec.edu.pe.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="Users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	private String username;
+	private String password;
+	private String email;
+	private String firstName;
+	private String lastName;
+	private String phone;
+	private String imageUrl;
+}
