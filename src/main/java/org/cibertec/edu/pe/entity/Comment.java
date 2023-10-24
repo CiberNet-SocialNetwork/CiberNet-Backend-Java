e0905fd304969c62741b37e748438746b381fca1
alexandramrodriguez <alexandramrodriguez25@)gmail.com>
2023-10-23 22:03:59 -0500

    SCRUM-16 add response login create

--- src/main/java/org/cibertec/edu/pe/entity/Comment.java
+++ src/main/java/org/cibertec/edu/pe/entity/Comment.java
@@ -24,4 +24,35 @@ public class Comment {
 	@ManyToOne
 	@JoinColumn(name="userId")
 	private User user;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getPublicationDate() {
+		return publicationDate;
+	}
+	public void setPublicationDate(Date publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+	public Post getPost() {
+		return post;
+	}
+	public void setPost(Post post) {
+		this.post = post;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
 }
